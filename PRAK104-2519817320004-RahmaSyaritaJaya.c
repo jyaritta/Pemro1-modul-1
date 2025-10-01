529fd53e83bdd9f06f22cd7d1116ad41526af776
jyaritta <rahmasyaritazaya@gmail.com>
2025-10-01 12:04:27 +0800

    Upload C File

--- PRAK104-2519817320004-RahmaSyaritaJaya.c
+++ PRAK104-2519817320004-RahmaSyaritaJaya.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    int A = 40000, B = 35000;
+    int A = 400000, B = 350000;
     float a = 0.13, b = 0.21;
     
     printf("Harga sepatu A adalah %d\nHarga sepatu A adalah %d\n", A, B);
     printf("Sepatu A mendapat diskon %.0f%% sehingga harganya menjadi %.0f\n", a*100, A*(1-a));
     printf("Sepatu B mendapat diskon %.0f%% sehingga harganya menjadi %.0f", b*100, B*(1-b));
     return 0;
+
 }
