59eaaa1935a30052e87f1b233e25176042eabf9c
jyaritta <rahmasyaritazaya@gmail.com>
2025-10-01 12:04:54 +0800

    Upload Python File

--- PRAK104-2519817320004-RahmaSyaritaJaya.py
+++ PRAK104-2519817320004-RahmaSyaritaJaya.py
@@ -1,6 +1,7 @@
-A, B = 40000, 35000
+A, B = 400000, 350000
 a, b = 0.13, 0.21
     
 print(f"Harga sepatu A adalah {A}\nHarga sepatu A adalah {B}")
 print(f"Sepatu A mendapat diskon {a*100:.0f}% sehingga harganya menjadi {A*(1-a):.0f}")
+
 print(f"Sepatu B mendapat diskon {b*100:.0f}% sehingga harganya menjadi {B*(1-b):.0f}")
