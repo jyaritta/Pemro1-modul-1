a888d85234e69d3ff55f0d04db3b1fb68946dad2
jyaritta <rahmasyaritazaya@gmail.com>
2025-10-13 14:36:48 +0800

    Upload C File

--- PRAK108-2519817320004-RahmaSyaritaJaya.c
+++ PRAK108-2519817320004-RahmaSyaritaJaya.c
@@ -4,8 +4,7 @@ int main(){
     float a = 5, b = 14;
 
     printf("Diketahui: \n");
-    printf("Pak Dengklek mengelilingi taman = %f Putaran\nJarak tempuh  Pak Dengklek = %f Kilometer\n\n", a, b);
-    
+    printf("Pak Dengklek mengelilingi taman = %f Putaran\nJarak tempuh  Pak Dengklek = %f Kilometer\n\n", a, b); 
     printf("Jawaban: \nJari-jari taman yang dikelilingi Pak Dengklek adalah %.2f Kilometer", (b/a)/(2 * 3.14));
     return 0;
 }
