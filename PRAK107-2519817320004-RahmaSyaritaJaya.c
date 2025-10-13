044f72b48d7f04c4a41edbead26d4edafafb00ec
jyaritta <rahmasyaritazaya@gmail.com>
2025-10-13 14:35:50 +0800

    Upload C File

--- PRAK107-2519817320004-RahmaSyaritaJaya.c
+++ PRAK107-2519817320004-RahmaSyaritaJaya.c
@@ -3,11 +3,11 @@
 int main(){
     int a = 4, b = 5, c = 7, k = a+b+c, h = 85000;
 
-    printf("Diketahui: ");
+    printf("Diketahui: \n");
     printf("Panjang sisi segitiga berturut-turut adalah %d, %d, %d\n", a, b, c);
     printf("Keliling Tanah Pak Dengklek adalah %d\n", k);
     printf("Harga tanah Per Meter adalah %d\n", h);
+    printf("Jawaban: \n");
     printf("Biaya yang diperlukan Pak Dengklek adalah: Rp %d", h*k);
     return 0;
-
 }
